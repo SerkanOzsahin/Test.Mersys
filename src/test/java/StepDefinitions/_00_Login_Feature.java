29ae75ebddd40fee356fdf330aeca1a63db15a45
NurhayatSoydan <141257552+NurhayatSoydan@users.noreply.github.com>
2023-10-16 12:59:22 +0300

    commit2

---
+++ src/test/java/StepDefinitions/_00_Login_Feature.java
@@ -0,0 +1,24 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _00_Login_Feature {
+    @Given("Navigate to Campus")
+    public void navigateToCampus() {
+    }
+
+    @When("The Enter username and password")
+    public void theEnterUsernameAndPassword() {
+    }
+
+    @Then("The User should click login button")
+    public void theUserShouldClickLoginButton() {
+    }
+
+    @And("The User should login successfully")
+    public void theUserShouldLoginSuccessfully() {
+    }
+}
