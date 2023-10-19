4c92309e2102d5cc4551990c058c87a8e5737683
aycadogannn <aycauresin@gmail.com>
2023-10-19 15:32:06 +0300

    finish almost half part

---
+++ src/test/java/Pages/DialogContent/_08_Department_ConfigurationDC.java
@@ -0,0 +1,51 @@
+package Pages.DialogContent;
+
+import Pages.Parent;
+import Utilities.BD;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class _08_Department_ConfigurationDC extends Parent {
+    public _08_Department_ConfigurationDC() {
+        PageFactory.initElements(BD.getDriver(), this);
+    }
+
+    @FindBy(css = "input[formcontrolname='username']")
+    public WebElement username;
+
+    @FindBy(css = "input[formcontrolname='password']")
+    public WebElement password;
+
+    @FindBy(css = "button[aria-label='LOGIN']")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//ms-add-button[contains(@tooltip,'ADD')]//button")
+    public WebElement addButton;
+
+    @FindBy(xpath = "(//input[@data-placeholder='Name'])[2]")
+    public WebElement departmentName;
+
+    @FindBy(xpath = "//input[@id='ms-text-field-15']")
+    public WebElement code;
+
+    @FindBy(xpath = "//ms-save-button/button")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "//div[contains(text(),'successfully')]")
+    public WebElement successMessage;
+
+    @FindBy(xpath = "//ms-edit-button")
+    public WebElement edit;
+
+    @FindBy(xpath = "(//ms-delete-button//button)[1]")
+    public WebElement deleteImageButton;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    public WebElement deleteDialogButton;
+
+    @FindBy( css= "")
+    public WebElement activeButton;
+    @FindBy( css= "")
+    public WebElement disactiveButton;
+}
