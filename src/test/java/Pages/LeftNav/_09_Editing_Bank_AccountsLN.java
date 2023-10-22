6e1dfc5d8f7c7f57f6fd86d73dd4b33c6aa0d986
iremtorun <irmtorun@gmail.com>
2023-10-22 18:34:28 +0300

    10

---
+++ src/test/java/Pages/LeftNav/_09_Editing_Bank_AccountsLN.java
@@ -0,0 +1,7 @@
+package Pages.LeftNav;
+
+import Pages.Parent;
+
+public class _09_Editing_Bank_AccountsLN extends Parent {
+
+}
---
+++ src/test/java/StepDefinitions/_09_Editing_Bank_Accounts.java
@@ -0,0 +1,22 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _09_Editing_Bank_Accounts {
+    @When("the user navigates to bank accounts")
+    public void theUserNavigatesToBankAccounts() {
+    }
+
+    @Then("the bank accounts should be added successfully")
+    public void theBankAccountsShouldBeAddedSuccessfully() {
+    }
+
+    @Then("the bank accounts should be edited successfully")
+    public void theBankAccountsShouldBeEditedSuccessfully() {
+    }
+
+    @Then("the bank accounts should be deleted successfully")
+    public void theBankAccountsShouldBeDeletedSuccessfully() {
+    }
+}
